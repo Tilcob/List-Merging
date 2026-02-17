9f301e3cf3705241938e71cf534caaa8cc624528
Jakob Dorninger <jakobdorninger19@gmail.com>
2026-02-17 11:35:35 +0100

    Integrate CSV and Excel merging capabilities with logging support.

--- src/main/java/github/tilcob/app/listmerging/controller/MainController.java
+++ src/main/java/github/tilcob/app/listmerging/controller/MainController.java
@@ -1,34 +1,47 @@
 package github.tilcob.app.listmerging.controller;
 
+import com.opencsv.exceptions.CsvValidationException;
 import github.tilcob.app.listmerging.service.HeaderLoader;
+import github.tilcob.app.listmerging.service.MergeService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class MainController {
+    private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
     @FXML
     private Label outputLabel;
 
     @FXML
     protected void onButtonMergeClick() {
         FileChooser fileChooser = createFileChooser("Select files to merge");
+        MergeService mergeService = new MergeService();
+        Map<List<String>, Integer> merged;
+
         List<File> files = fileChooser.showOpenMultipleDialog(getCurrentWindow());
+
         if (Objects.isNull(files) || files.isEmpty()) {
             outputLabel.setText("No files selected");
             return;
         }
         try {
-            HeaderLoader loader = new HeaderLoader();
-        } catch (IOException e) {
+            merged = Map.copyOf(mergeService.merge(files));
+        } catch (Exception e) {
             outputLabel.setText("Error loading headers");
-            throw new UncheckedIOException(e);
+            log.error("Error loading headers: ", e);
         }
     }
 
